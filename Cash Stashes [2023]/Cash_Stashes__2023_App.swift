3625b04506cd19395bb22d404bbd97972ee907dc
LuckDev <lukedru@yahoo.com>
2022-12-12 11:02:57 -0800

    Initial Commit

---
+++ Cash Stashes [2023]/Cash_Stashes__2023_App.swift
@@ -0,0 +1,17 @@
+//
+//  Cash_Stashes__2023_App.swift
+//  Cash Stashes [2023]
+//
+//  Created by Luke Drushell on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Cash_Stashes__2023_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
